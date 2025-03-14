7087bae5c8cd3fcffcb50f6d5f642d086bb938d4
danielUTM <danielcampbell.327@icloud.com>
2025-03-14 20:19:22 +0000

    Add support for one-character and two-character tokens as well as new key word tokens

--- token/token.go
+++ token/token.go
@@ -20,13 +20,32 @@ const (
 	RPAREN    = ")"
 	LBRACE    = "{"
 	RBRACE    = "}"
-	FUNCTION  = "FUNCTION"
-	LET       = "LET"
+	BANG      = "!"
+	MINUS     = "-"
+	SLASH     = "/"
+	ASTERISK  = "*"
+	LT        = "<"
+	GT        = ">"
+	EQ        = "=="
+	NOT_EQ    = "!="
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
